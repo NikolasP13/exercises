55ccce9fe1738b82dc6952ddd5637759153ed33c
NikolasP13 <100437071+NikolasP13@users.noreply.github.com>
2022-03-14 11:03:19 -0400

    debut de lexercise

---
+++ 14-03-2022-excer.py
@@ -0,0 +1,9 @@
+#Créer une fonction prenant 4 paramètres et les retournant en
+#ordre croissant en utilisant que des conditions(en n’utilisant pas de loops)
+
+def ordre_croissant():
+    number1 = float(input)('nombre1')
+    number2 = float(input)('nombre2')
+    number3 = float(input)('nombre3')
+    number4 = float(input)('nombre4')
+    if number1 > number2 
